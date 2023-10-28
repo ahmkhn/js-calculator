98fc72c036202fbe132d2e1eeeb810c633ccda4e
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2023-10-28 06:41:58 -0400

    basic functions & setup complete.

---
+++ index.js
@@ -0,0 +1,36 @@
+// Let's start by creating basic functions for the operations of a calculator.
+
+function add(a,b)
+{
+    return(a+b);
+}
+function subtract(a,b)
+{
+    return (a-b)
+}
+function multiply(a,b)
+{
+    return(a*b);
+}
+function divide(a,b)
+{
+    return(a/b);
+}
+// basic functions done.
+function operate(operator,num1,num2)
+{
+    if(operator=="+")
+    {
+        return(add(num1,num2));
+    } else if(operator=="-")
+    {
+        return(subtract(num1,num2));
+    } else if(operator=="/")
+    {
+        return(divide(num1,num2));
+    }else if(operator=="*")
+    {
+        return(multiply(num1,num2));
+    }
+}
+// operator function complete.
